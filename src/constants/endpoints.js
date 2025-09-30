6ca6a1e01b63563b53a5999551a2674043f4e5fb
geduma <gedumarino@gmail.com>
2025-09-30 03:14:53 -0500

    feat(endpoints): add TELEGRAM_DELETE_MESSAGE endpoint and update imageUrlToBase64 function

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -2,5 +2,6 @@ export const Endpoints = {
   GITHUB_ACCESS_TOKEN: 'https://github.com/login/oauth/access_token',
   GITHUB_USER: 'https://api.github.com/user',
   TELEGRAM_GET_FILE: `https://api.telegram.org/bot${process.env.TELEGRAM_GEDUMABOT_TOKEN}/getFile`,
-  TELEGRAM_FILE_BASE_URL: `https://api.telegram.org/file/bot${process.env.TELEGRAM_GEDUMABOT_TOKEN}`
+  TELEGRAM_FILE_BASE_URL: `https://api.telegram.org/file/bot${process.env.TELEGRAM_GEDUMABOT_TOKEN}`,
+  TELEGRAM_DELETE_MESSAGE: `https://api.telegram.org/bot${process.env.TELEGRAM_GEDUMABOT_TOKEN}/deleteMessage`
 }
--- src/utils/imageUrlToBase64.js
+++ src/utils/imageUrlToBase64.js
@@ -15,16 +15,4 @@ export const imageUrlToBase64 = (imageUrl) => {
         reject(err)
       })
   })
-  // try {
-  //   const response = await fetch(imageUrl)
-  //   const arrayBuffer = await response.arrayBuffer()
-  //   const base64 = Buffer.from(arrayBuffer).toString('base64')
-  //   const mimeType = response.headers.get('content-type') || 'image/png'
-  //   console.log(`data:${mimeType};base64,${base64}`)
-
-  //   return `data:${mimeType};base64,${base64}`
-  // } catch (err) {
-  //   console.error('Error converting image URL to Base64:', err)
-  //   throw err
-  // }
 }
