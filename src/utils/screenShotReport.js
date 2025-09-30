ba075c2996dc9385dff95e30ff37ee1f9591ed41
geduma <gedumarino@gmail.com>
2025-09-30 11:21:13 -0500

    chore(screenshot-backup): format backupDate

--- src/utils/screenShotReport.js
+++ src/utils/screenShotReport.js
@@ -6,7 +6,7 @@ export const generateReport = (screenShotList) => {
 
   screenShotList.forEach(item => {
     let tableItem = '<tr><th>'
-    tableItem += `<p style="text-align: left;width: 100%;font-weight: 200;margin-top: 0;margin-bottom: 0;"><strong style="font-weight: 600;">Fecha: </strong>${item.backupDate}</p>`
+    tableItem += `<p style="text-align: left;width: 100%;font-weight: 200;margin-top: 0;margin-bottom: 0;"><strong style="font-weight: 600;">Fecha: </strong>${item.backupDateString}</p>`
     tableItem += `<p style="text-align: left;width: 100%;font-weight: 200;margin-top: 0;margin-bottom: 0;"><strong style="font-weight: 600;">Descripción: </strong>${item.textMessage}</p>`
     tableItem += `<img alt="screen-shot-backup-item" style="width: 60%;margin-top: 0.5rem;margin-bottom: 1rem;" src="${Endpoints.TELEGRAM_FILE_BASE_URL}/${item.filePath}"></th></tr>`
     html += tableItem
