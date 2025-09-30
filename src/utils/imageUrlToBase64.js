06c8f7e1a388195e2a9ebfdbd6aaecc947deeb56
geduma <gedumarino@gmail.com>
2025-09-30 01:50:26 -0500

    chore(screenshot-backup): add temporal logs

--- src/utils/imageUrlToBase64.js
+++ src/utils/imageUrlToBase64.js
@@ -4,6 +4,7 @@ export const imageUrlToBase64 = async (imageUrl) => {
     const arrayBuffer = await response.arrayBuffer()
     const base64 = Buffer.from(arrayBuffer).toString('base64')
     const mimeType = response.headers.get('content-type') || 'image/png'
+    console.log(`data:${mimeType};base64,${base64}`)
 
     return `data:${mimeType};base64,${base64}`
   } catch (err) {
