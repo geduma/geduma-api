d50f965f99066e69ab6fb4ba2c4e8317540adba0
geduma <gedumarino@gmail.com>
2025-10-31 01:02:33 -0500

    feat: integrate Upstash Redis for token management in security interceptor

--- src/interceptors/security.interceptor.js
+++ src/interceptors/security.interceptor.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
+import { Redis } from '@upstash/redis'
 import { v4 as uuidv4 } from 'uuid'
 import { generalResponse } from '../utils/generalResponse.js'
 
@@ -19,34 +20,48 @@ const apiSecrets = {
   'screenshot-backup': process.env.API_SCREENSHOT_BACKUP_TOKEN_SECRET
 }
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN
+})
+
 const auth = ({ name, user, key, data = { user } }) => {
   const jti = uuidv4()
   return new Promise((resolve, reject) => {
     if (key !== apiKeys[name]) reject(Error('invalid or missing api key'))
-    resolve({
-      token: jwt.sign({
-        data,
-        jti
-      }, apiSecrets[name], { expiresIn: '5m' })
-    })
+    const token = jwt.sign({
+      data,
+      jti
+    }, apiSecrets[name], { expiresIn: '5m' })
+
+    redis.set(jti, token)
+      .then(() => resolve({ token }))
   })
 }
 
 const verify = (req, res, next) => {
-  const result = verifyJWT({
-    token: req.headers.authorization,
+  const token = req.headers.authorization
+  verifyJWT({
+    token,
     apiSecret: apiSecrets[req.url.split('/')[1]]
+  }).then((valid) => {
+    if (valid) {
+      redis.del(jwt.decode(token.split(' ')[1]).jti)
+        .then(() => next())
+    } else res.status(401).send(generalResponse.error('Unauthorized: invalid or missing token'))
   })
-  if (result) next()
-  else res.status(401).send(generalResponse.error('Unauthorized: invalid or missing token'))
 }
 
-const verifyJWT = ({ token, apiSecret }) => {
+const verifyJWT = async ({ token, apiSecret }) => {
   let res = true
+  const jwtDecoded = jwt.decode(token.split(' ')[1])
+  const storedToken = await redis.get(jwtDecoded.jti)
+
   try {
     if (!token) res = false
+    if (!storedToken) res = false
+
     jwt.verify(token.split(' ')[1], apiSecret)
-    // if (usedTokens.has(decode.jti)) throw new Error('token already used')
   } catch (error) {
     res = false
   }
